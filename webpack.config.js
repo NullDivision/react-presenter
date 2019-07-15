c273fcebfd19f00867bc25ae9e79e22b4623e5bc
Tobias Elod-Zoltan <etobias@pentalog.com>
2019-07-15 13:38:08 +0300

    feat(package): update babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   devtool: 'eval-source-map',
+  entry: './src/index.js',
   mode: 'development',
   module: {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       { test: /\.js$/, use: ['babel-loader', 'eslint-loader'] }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin()]
 };
